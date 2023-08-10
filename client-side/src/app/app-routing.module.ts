aa2584a14bc64c61276777661e1a56a186ffd1d7
TrioNxtIndia <88088329+TrioNxtIndia@users.noreply.github.com>
2023-08-10 12:03:08 +0530

    Add files via upload

---
+++ client-side/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerRegistrationComponent } from './customer-registration/customer-registration.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'list' },
+  {
+    path: "register", component: CustomerRegistrationComponent 
+  },
+  {
+    path: "list", component: CustomerListComponent 
+  },
+  {
+    path: "detail/:id", component: CustomerDetailComponent 
+  },
+  {
+    path: "update/:id", component: CustomerRegistrationComponent 
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ client-side/src/app/models/customer.model.ts
@@ -0,0 +1,10 @@
+export class Customer {
+    id!: number;
+    name!: string;
+    email!: string;
+    mobile!: number;
+    project!: string;
+    sale_contact!:string;
+    status!: string;
+    golivedate!: string;
+}
